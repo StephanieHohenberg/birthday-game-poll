560fa96692ad6b7f69f7c18d45384ada3d61b02c
hohenberg <stephanie.hohenberg@adesso.de>
2020-09-09 18:21:33 +0200

    deploys new version

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import {PollFormComponent} from './components/poll-form/poll-form.component';
 import {GameComponent} from './components/game/game.component';
 
 export const ROUTES_CONFIG = [
-  {path: '', component: PollFormComponent},
+  {path: '', component: GameComponent},
   {path: 'poll', component: PollFormComponent},
   {path: 'session', component: GameComponent},
   {path: '**', redirectTo: '/'}
